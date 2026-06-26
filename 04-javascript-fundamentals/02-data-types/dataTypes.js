2cbea27d3558f69fc21f68f407f4cf036a3d4e66
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-06-26 17:26:03 +0530

    feat: add data types in javascript

---
+++ 04-javascript-fundamentals/02-data-types/dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as newer version
+
+let studentName = "Tejas";               // string
+const rollNumber = 77;                   // number
+let applicationNumber = 12345678910;     // bigint
+let isLoggedIn = true;                   // boolean
+let score;                               // undefined
+let assignment = null;                   // null
+
+// type of a null data type is object
+
+console.log(typeof studentName);
+console.log(typeof rollNumber);
+console.log(typeof applicationNumber);
+console.log(typeof isLoggedIn);
+console.log(typeof score);
+console.log(typeof assignment);
