bc8957acfdd47d464c37988912db245a449ad92f
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-06-27 14:10:09 +0530

    feat: add conversion of datatypes in js

---
+++ 04-javascript-fundamentals/03-type-conversion/conversion.js
@@ -0,0 +1,36 @@
+let score = "100"
+console.log(typeof score);
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+
+let marks = "26sep"
+console.log(typeof marks);
+
+let marksInNumber = Number(marks);
+console.log(typeof marksInNumber);
+console.log(marksInNumber);            
+// it gives a NaN(not a number) as a output
+// NaN is a special type in js
+
+let temp = null
+console.log(typeof temp);
+
+let tempInNumber = Number(temp)
+console.log(typeof tempInNumber);
+console.log(tempInNumber);
+
+let alfa = "abcd"
+console.log(typeof alfa);
+
+let alfaInNumber = Number(alfa);
+console.log(alfaInNumber);
+// we can't able to change string to number it gives NaN as a conversion
+// But we can change number to string
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1 ; false = 0
+// 1 = true; 0 = false
+// "" = false 
+// "tejas" = true 
