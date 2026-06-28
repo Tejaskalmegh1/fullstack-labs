3686229caf1d8934071ce83f7da92c8eab6c71cb
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-06-28 14:51:52 +0530

    feat: add assignment operators in javascript

---
+++ 04-javascript-fundamentals/04-operators/assignment-operators.js
@@ -0,0 +1,41 @@
+// Assignment Operators
+// ( = , += , -= , *= , /= , %= , **= )
+
+// Assignment
+let numberOne = 10;
+let numberTwo = numberOne; // numberOne = 10, numberTwo = 10
+console.log("first number: ",numberOne, " second number: ", numberTwo);
+
+
+// Addition Assignment
+numberOne = 10, numberTwo = 5;
+numberOne += numberTwo;             // numberOne = numberOne + numberTwo
+console.log("first number: ",numberOne, " second number: ", numberTwo);
+
+
+// Subtraction Assignment
+numberOne = 10, numberTwo = 5;
+numberOne -= numberTwo;             // numberOne = numberOne - numberTwo
+console.log("first number: ",numberOne, " second number: ", numberTwo);
+
+
+// Multiplication Assignment
+numberOne = 10, numberTwo = 5;
+numberOne *= numberTwo;             // numberOne = numberOne * numberTwo
+console.log("first number: ",numberOne, " second number: ", numberTwo);
+
+// Division Assignment
+numberOne = 10, numberTwo = 5;
+numberOne /= numberTwo;             // numberOne = numberOne / numberTwo
+console.log("first number: ",numberOne, " second number: ", numberTwo);
+
+// Remainder Assignment
+numberOne = 10, numberTwo = 5;
+numberOne %= numberTwo;             // numberOne = numberOne % numberTwo
+console.log("first number: ",numberOne, " second number: ", numberTwo);
+
+// Exponentiation Assignment
+numberOne = 10, numberTwo = 5;
+numberOne **= numberTwo;            // numberOne = numberOne ** numberTwo
+console.log("first number: ",numberOne, " second number: ", numberTwo);
+
