c7e07628c4fa4abbb93ca55744c6383c31cca8c0
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-06-28 18:25:14 +0530

    feat: add ternary operator in javascript

---
+++ 04-javascript-fundamentals/04-operators/ternary-operator.js
@@ -0,0 +1,9 @@
+// Ternary (Conditional) Operator 
+
+// Syntax: condition ? exprIfTrue : exprIfFalse
+
+let age = 19
+
+let isEligible = (age >= 18) ? "Eligible" : "Not Eligible" ;
+
+console.log("student is ",isEligible," to vote");
