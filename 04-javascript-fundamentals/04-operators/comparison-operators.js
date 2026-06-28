940af3e53d259b29b0343ca84942bf9ab9cf2359
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-06-28 19:07:41 +0530

    feat: add comparison operator in javascript

---
+++ 04-javascript-fundamentals/04-operators/comparison-operators.js
@@ -0,0 +1,59 @@
+// Comparison Operators
+// ( == , === , != , !== , < , > , <= , >= )
+
+let numberOne = 10
+let numberTwo = "10"
+let output
+
+// Equality operator (==)
+// checks whether two values are equal. If the values have different data types, it converts them before comparing. Returns true if they are equal; otherwise, false
+
+output = (numberOne == numberTwo) ? true : false ;
+console.log(output);
+
+
+// Inequality operator (!=)
+// Checks whether two values are not equal. If the values have different data types, it converts them before comparing. Returns true if they are not equal; otherwise, false
+
+output = (numberOne != numberTwo) ? true : false ;
+console.log(output);
+
+
+// Strict Equality Operator (===)
+// Checks whether two values are different in value or data type. It returns true if either the values or their types are different; otherwise, false
+
+output = (numberOne === numberTwo) ? true : false ;
+console.log(output);
+
+
+// Strict Inequality Operator  (!==)
+// Checks if two values or their data types are different. It returns true if the values or types differ; otherwise, false
+
+output = (numberOne === numberTwo) ? true : false ;
+console.log(output);
+
+// Greater Than Operator (>) 
+// Checks if the left value is greater than the right value. Returns true if it is; otherwise, false
+
+output = (numberOne > numberTwo) ? true : false ;
+console.log(output);
+
+
+// Greater Than or Equal Operator (>=) 
+// Checks if the left value is greater than or equal to the right value. Returns true if it is; otherwise, false
+
+output = (numberOne >= numberTwo) ? true : false ;
+console.log(output);
+
+
+// Less Than Operator (<) 
+// Checks if the left value is less than the right value. Returns true if it is; otherwise, false
+
+output = (numberOne < numberTwo) ? true : false ;
+console.log(output);
+
+// Less Than or Equal Operator (<=) 
+// Checks if the left value is less than or equal to the right value. Returns true if it is; otherwise, false
+
+output = (numberOne <= numberTwo) ? true : false ;
+console.log(output);
