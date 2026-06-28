d7fbfdb9bc4355e27240bdd56f8965d1d394448f
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-06-28 14:17:13 +0530

    feat: add arithmetic operators in javascript

---
+++ 04-javascript-fundamentals/04-operators/arithmetic-operators.js
@@ -0,0 +1,48 @@
+// Arithmetic Operators
+
+let numberOne = 10;
+let numberTwo = 5;
+
+// Addition operator
+console.log("Addition : ",numberOne + numberTwo);
+
+// Subtraction operator
+console.log("Subtraction : ",numberOne - numberTwo);
+
+//  Multiplication operator
+console.log("Multiplication : ",numberOne * numberTwo);
+
+//  Division operator
+console.log("Division : ",numberOne / numberTwo);
+
+//  Modulus operator
+// modulus operator returns the remainder when first operand is divided by the second operand
+console.log("Modulus : ",numberOne % numberTwo);
+
+//  Increment operator
+let numberThree = 10
+
+// Prefix Increment Operator :
+// The prefix increment operator increments the value of the variable before its current value is used
+console.log(numberThree);    // numberThree = 10
+console.log(++numberThree);  // numberThree = 11
+
+// Postfix Increment Operator :
+// The postfix increment operator increments the value of the variable after its current value is used
+console.log(numberThree++);  // numberThree = 11
+console.log(numberThree);    // numberThree = 12
+
+
+//  Decrement operator
+let numberFour = 10
+
+// Prefix Decrement Operator :
+// The prefix decrement operator decrements the value of the variable before its current value is used
+console.log(numberFour);   // numberFour = 10
+console.log(--numberFour); // numberFour = 9
+
+// Postfix Decrement Operator :
+// The postfix decrement operator decrements the value of the variable after its current value is used
+console.log(numberFour--); // numberFour = 9
+console.log(numberFour);   // numberFour = 8
+
