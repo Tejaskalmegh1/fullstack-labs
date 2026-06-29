fd9ec11e4139e424bd3f23ca7cf4aaa12a009475
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-06-29 16:13:30 +0530

    feat: add while loop in javascript

---
+++ 04-javascript-fundamentals/06-loops/while-loop.js
@@ -0,0 +1,15 @@
+// while loop
+
+// syntax:
+// while (expression) {
+//    Statement(s) to be executed if expression is true
+// }
+
+// A while loop executes a block of code repeatedly as long as the specified condition remains true
+
+let n = 1
+
+while (n <= 5) {
+    console.log("This is while loop...");
+    n++;
+}
