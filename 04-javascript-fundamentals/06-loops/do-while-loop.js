a48112f113b7b6f17da580af6810d00798b62984
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-06-29 16:14:01 +0530

    feat: add do while loop in javascript

---
+++ 04-javascript-fundamentals/06-loops/do-while-loop.js
@@ -0,0 +1,15 @@
+// do while loop
+
+// syntax 
+// do {
+//    Statement(s) to be executed;
+// } while (expression);
+
+// A do...while loop executes a block of code once before checking the condition, then continues repeating as long as the condition is true
+
+let num = 5;
+
+do {
+  console.log(num);
+  num--;
+} while (num > 0);
