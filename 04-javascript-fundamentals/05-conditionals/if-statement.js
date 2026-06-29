7b37c67910ae81476757fd967e31e87b9c148754
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-06-29 12:55:14 +0530

    feat: add if statement in javascript

---
+++ 04-javascript-fundamentals/05-conditionals/if-statement.js
@@ -0,0 +1,15 @@
+// If statement 
+
+// syntax: 
+// if (expression) {
+//    Statement(s) to be executed if expression is true
+// }
+
+// The if() statement executes a block of code only if the condition evaluates to true
+
+let numberOne = 10;
+let numberTwo = 5;
+
+if ( numberOne > numberTwo ) {
+    console.log(numberOne, "is greater...");
+}
