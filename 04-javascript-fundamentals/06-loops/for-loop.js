1a728cc504347da73ff3849398d2fcac85caccf8
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-06-29 16:12:33 +0530

    feat: add for loop in javascript

---
+++ 04-javascript-fundamentals/06-loops/for-loop.js
@@ -0,0 +1,12 @@
+// for loop 
+
+// syntax: 
+// for (initialization; condition; iteration) {
+//    Statement(s) to be executed if condition is true
+// }
+
+// A for loop is used to execute a block of code repeatedly until a specified condition becomes false
+
+for (let i = 1; i <= 5; i++) {
+  console.log("This is for loop...");
+}
