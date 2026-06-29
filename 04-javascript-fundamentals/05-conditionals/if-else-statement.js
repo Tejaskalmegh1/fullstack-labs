7caed7b372a1ff40ccf30addf834b4105b589e20
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-06-29 13:00:53 +0530

    feat: add if-else statement in javascript

---
+++ 04-javascript-fundamentals/05-conditionals/if-else-statement.js
@@ -0,0 +1,21 @@
+// if else statement
+
+// syntax: 
+// if (expression) {
+//    Statement(s) to be executed if expression is true
+// } else {
+//    Statement(s) to be executed if expression is false
+// }
+
+// The if else Statement Executes one block if the condition is true, and another if it's false
+
+let numberOne = 10;
+let numberTwo = 15;
+
+if ( numberOne > numberTwo ) {
+    console.log(numberOne, "is greater...");
+}
+else {
+    console.log(numberTwo, "is greater...");
+}
+
