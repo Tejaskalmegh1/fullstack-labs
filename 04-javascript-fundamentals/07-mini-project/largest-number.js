8adc537aa4667de21dcac4564b727f2849589698
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-06-29 17:30:24 +0530

    feat: add largest number program in javascript

---
+++ 04-javascript-fundamentals/07-mini-project/largest-number.js
@@ -0,0 +1,9 @@
+let numberOne = 15
+let numberTwo = 10
+
+if(numberOne > numberTwo) {
+    console.log(numberOne,"is greater than",numberTwo);
+}
+else {
+    console.log(numberTwo,"is greater than",numberOne);
+}
