346b82200c32538dffb13a0862dfac3bd03d2c10
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-06-29 17:46:20 +0530

    feat: add calculator program in javascript

---
+++ 04-javascript-fundamentals/08-calculator-project/calculator.js
@@ -0,0 +1,28 @@
+let numberOne = 10;
+let numberTwo = 5;
+
+let ch = 1;
+
+switch(ch) {
+
+        case 1:
+        console.log("Addition:",numberOne+numberTwo);
+        break;
+        
+        case 2:
+        console.log("Sub:",numberOne+numberTwo);
+        break;
+        
+        case 3:
+        console.log("Multi:",numberOne*numberTwo);
+        break;
+        
+        case 4:
+        console.log("Div:",numberOne/numberTwo);
+        break;
+        
+        default:
+        console.log("Invalid choice...");
+        break;
+        
+}
